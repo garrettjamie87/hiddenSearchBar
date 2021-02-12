ca3e50ab27d60354dcc9b650d318d8718310dfb6
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-12 15:47:23 +0100

    save querySelctor into variables

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchBar = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const searchBar = document.querySelector('.input');
+
+
+
+
+
+
+// function addActiveClass() {
+// searchBar.addEventListener("click", ()=> {
+//       searchBar.classList.add('active')
+// } );
+// }
+
+// addActiveClass()
+
+// function removeActiveClass() {
+//       searchBar.addEventListener("click", ()=> {
+//             searchBar.classList.remove('active')
+//       } );
+//       }
+//       removeActiveClass()
