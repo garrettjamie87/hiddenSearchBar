95888389343eb5639838e58c7f1a170d961256df
Jamie Garrett <garrettjamie87@googlemail.com>
2021-02-12 16:15:29 +0100

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const searchBar = document.querySelector('.search');
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
+const searchBar = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
 
-
-btn.addEventListener('click', ()=>{
-      searchBar.classList.toggle('active')
-      input.focus()
-})
-
-
-
+btn.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+  input.focus();
+});
